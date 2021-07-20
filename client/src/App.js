458edfe1cf9880a67cf2fb82dac2157c673324af
sam west <samwest0230@gmail.com>
2021-07-20 15:26:14 -0400

    added the foundation of the front end to test backend api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,92 @@
+import React from "react"
+import './App.scss'
+import axios from 'axios'
+const url = process.env.REACT_APP_URL
 
-import './App.css';
+class App extends React.Component {
+  state = {
+   games: [],
 
-function App() {
-  return (
-    <div className="App">
-    <h1>hey</h1>
-    </div>
-  );
+}
+
+  handleChange = (e) => {
+    console.log(e.target.value)
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+
+    get = () => {
+      axios
+      .get(url + '/')
+      .then(response => {
+        this.setState({
+          games: response
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+
+
+  render() {
+    return (
+      <div className="app">
+        <h1 className='app__title'>Test API</h1>
+
+        <div className='app__all'>
+          <h2 className='app__all--title'>Get Games</h2>
+          <button className='app__all--button' onClick={this.get}>GET</button>
+        </div>
+
+        <div className='app__games'>
+          {this.state.games.forEach(game => {
+            return(
+              <div className='app__games--div'> 
+                <h1>{game.name}</h1>
+                <h2>{game.platform}</h2>
+                <h3>{game.date}</h3>
+                <h4>{game.id}</h4>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className='app__add'>
+          <h2>Add Game</h2>
+          <form>
+            <label>Name of Game</label>
+            <input type='text' name='name'></input>
+            <label>Platform</label>
+            <input type='text' name='platform'></input>
+            <label>Release Date</label>
+            <input type='date' name='date'></input>
+            <button>Add</button>
+          </form>
+        </div>
+
+        <div className='app__find'>
+          <h2>Find Game</h2>
+          <form>
+            <label>Name of Game</label>
+            <input type='text' name='name'></input>
+            <button>Find</button>
+          </form>
+        </div>
+
+        <div className='app__delete'>
+          <h2> Delete Game</h2>
+          <form>
+            <label>Game ID</label>
+            <input type='number' name='id'></input>
+            <button>Delete</button>
+          </form>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- routes/games.js
+++ routes/games.js
@@ -4,6 +4,30 @@ const router = express.Router();
 const uniqid = require("uniqid");
 
 
+router.get("/", (req, res) => {
 
+  });
+  
+ 
+  router.get("", (req, res) => {
+   
+  });
+  
+
+  router.post("", (req,res) => {
+    res.send("");
+  })
+  
+
+  router.put("", (req, res) => {
+    
+    res.send("")
+  });
+  
+ 
+  router.delete("", (req, res) => {
+  
+    res.send();
+  })
 
 module.exports = router;
